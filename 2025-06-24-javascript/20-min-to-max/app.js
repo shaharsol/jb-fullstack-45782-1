dfbea2a4a81fa8d84a6e3480e58693ed0d0b7884
shaharsol <shahar@gnift.com>
2025-06-24 15:14:12 +0300

    for 7

--- 2025-06-24-javascript/20-min-to-max/app.js
+++ 2025-06-24-javascript/20-min-to-max/app.js
@@ -1,3 +1,16 @@
-for(let count = -100; count <= 100; count += 2) {
+let min = +prompt('enter number')
+let max = +prompt('enter number')
+
+if (min > max) {
+    let helper = min;
+    min = max;
+    max = helper;
+}
+
+for(let count = min; count <= max; count ++) {
     document.write(`${count}<br>`)
 }
+
+// for(; min <= max; min ++) {
+//     document.write(`${min}<br>`)
+// }
---
+++ 2025-06-24-javascript/22-ten-by-ten-asteriks/app.js
@@ -0,0 +1,3 @@
+for(let current = 7; current <= 100; current += 7) {
+    document.write(`${current**2}<br>`)
+}
