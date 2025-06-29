1fca6fbbf14f79565d45e799492ab9f353c96c7a
shaharsol <shahar@gnift.com>
2025-06-29 13:27:22 +0300

    literal object

--- 2025-06-29-javascript/06-random-10-30/app.js
+++ 2025-06-29-javascript/06-random-10-30/app.js
@@ -3,3 +3,4 @@ const random = Math.random(); // 0-0.999999
 console.log(random * 21); // 0.00 - 20.99
 console.log(parseInt(random * 21)); // 0 - 20
 console.log(parseInt(random * 21) + 10); // 10 - 30
+
---
+++ 2025-06-29-javascript/09-literal-object/app.js
@@ -0,0 +1,80 @@
+// description of betty:
+const dogName = "Betty";
+const dogAge = 3;
+const dogBreed = "mixed";
+const dogGender = "male";
+const dogWeight = 20;
+
+//
+const dog = {
+  name: "Betty",
+  age: 3,
+  breed: "mixed",
+  gender: "female",
+  weight: 20,
+};
+
+console.log(dog);
+
+// console.log(
+//   `my dog name is ${dogName} and ${
+//     dogGender === "male" ? "he" : "she"
+//   } weighs ${dogWeight}kg`
+// );
+
+// i can add properties dynamically
+dog.color = "grey";
+
+// dog.age is equivalent to dog['age']
+console.log(
+  `my dog name is ${dog.name} and ${
+    dog.gender === "male" ? "he" : "she"
+  } weighs ${dog.weight}kg, and the color is ${dog.color}, and she is ${
+    dog["age"]
+  } years old`
+);
+
+dog.color = "black";
+
+console.log(
+  `my dog name is ${dog.name} and ${
+    dog.gender === "male" ? "he" : "she"
+  } weighs ${dog.weight}kg, and the color is ${dog.color}, and she is ${
+    dog["age"]
+  } years old`
+);
+
+// objects as consts
+// this won't work:
+// dog = {
+//   age: 2,
+//   breed: "Doberman",
+//   color: "black",
+// };
+
+// however, i can ALWAYS modify the properties. Even if the object is defined as const
+dog.age = 2;
+dog.breed = "Doberman";
+dog.color = "black";
+
+console.log(
+  `my dog name is ${dog.name} and ${
+    dog.gender === "male" ? "he" : "she"
+  } weighs ${dog.weight}kg, and the color is ${dog.color}, and she is ${
+    dog["age"]
+  } years old`
+);
+
+// i can delete properties from an object
+console.log(dog);
+delete dog.age;
+console.log(dog);
+
+// access to non-existing properties always returns undefined
+console.log(`dog age is ${dog.age}`);
+
+// a special loop syntax to iterate properties:
+// for-in
+for (const property in dog) {
+  console.log(property);
+}
