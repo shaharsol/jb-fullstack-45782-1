67c5d9549991b282c4bbbc1d9a3549d7762e8ef9
shaharsol <shahar@gnift.com>
2025-07-01 11:16:58 +0300

    grades max min

---
+++ 2025-07-01-javascript/05-class-grades-max-min/app.js
@@ -0,0 +1,38 @@
+const ido = {
+  name: "Ido",
+  city: "rosh haayin",
+  grades: [90, 95, 88],
+};
+
+const lior = {
+  name: "Lior",
+  city: "arsuf ilit",
+  grades: [92, 91, 87],
+};
+
+const itay = {
+  name: "Itay",
+  city: "Haifa",
+  grades: [99, 94, 81],
+};
+
+const students = [ido, lior, itay];
+
+let max = 0;
+let min = 100;
+let maxStudent, minStudent;
+
+for (const student of students) {
+  for (const grade of student.grades) {
+    if (grade > max) {
+      max = grade;
+      maxStudent = student.name;
+    }
+    if (grade < min) {
+      min = grade;
+      minStudent = student.name;
+    }
+  }
+}
+
+console.log(`max is ${max} by ${maxStudent}, min is ${min} by ${minStudent}`);
