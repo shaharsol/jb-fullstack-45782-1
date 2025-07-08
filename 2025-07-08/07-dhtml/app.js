6c7d462c5c337e31a134405016f7c93ff3e74ef3
shaharsol <shahar@gnift.com>
2025-07-08 12:20:03 +0300

    dhtml

---
+++ 2025-07-08/07-dhtml/app.js
@@ -0,0 +1,12 @@
+function addFruit() {
+  const newFruit = document.getElementById("fruit").value;
+  const list = document.getElementById("list");
+  console.log(list);
+
+  const newLi = `<li>${newFruit}</li>`;
+
+  list.innerHTML = list.innerHTML + newLi;
+  list.style.backgroundColor = "red";
+
+  document.getElementById("fruit").value = "";
+}
