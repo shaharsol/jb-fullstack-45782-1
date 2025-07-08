f2328b4b9a40f58f840d4bf871c7f2dfad2b1178
shaharsol <shahar@gnift.com>
2025-07-08 11:16:47 +0300

    dhtml

---
+++ 2025-07-08/04-inner-text-vs-inner-html/app.js
@@ -0,0 +1,9 @@
+function changeText() {
+  document.getElementById("p1").innerText =
+    document.getElementById("text1").value;
+}
+
+function changeHTML() {
+  document.getElementById("p2").innerHTML =
+    document.getElementById("text2").value;
+}
