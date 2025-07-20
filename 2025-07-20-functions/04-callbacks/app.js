e0787b5331ed1379d39e9920916047a288b58287
shaharsol <shahar@gnift.com>
2025-07-20 09:51:13 +0300

    callbacks

--- 2025-07-20-functions/04-callbacks/app.js
+++ 2025-07-20-functions/04-callbacks/app.js
@@ -1,12 +1,24 @@
-function showGrade(grade) {
-    console.log(`your grade is ${grade}`)
-    return 'Yarden'
+function power2(num) {
+    return num ** 2
 }
 
-showGrade(96)
-console.log(showGrade(96))
+function power3(num) {
+    return num ** 3
+}
 
-showGrade = 'Gold'
 
-showGrade(96)
-console.log(showGrade(96))
+console.log(power2(3))
+console.log(power3(3))
+
+function somePower(num, func) {
+    if (typeof func !== 'function') alert('big error, no function was passed as argument')
+    console.log(`result of ${num} and ${func} is ${func(num)}`)
+}
+
+// somePower(4, 4)
+
+somePower(4, power2)
+
+somePower(4, power3)
+
+somePower(4, console.log)
