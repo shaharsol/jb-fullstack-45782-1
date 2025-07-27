6237f8dcdfd800ff5817ee5abb0bdc4bc668011c
shaharsol <shahar@gnift.com>
2025-07-27 13:26:08 +0300

    async

---
+++ 2025-07-27-async/06-callstack-callback-web-apis/app.js
@@ -0,0 +1,18 @@
+"use strict";
+
+(() => {
+
+    const a = () => {
+        setTimeout(() => {
+            alert('a')
+        }, 3000)
+    }
+
+
+    a();
+
+    for (let i = 0; i < 100000; i++) {
+        console.log(i)
+    }
+
+})()
