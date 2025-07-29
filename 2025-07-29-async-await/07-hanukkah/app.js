3b5f3f55efb66432fc1c1e41c5cd41d9107c0518
shaharsol <shahar@gnift.com>
2025-07-29 11:37:03 +0300

    hannukah

--- 2025-07-29-async-await/07-hanukkah/app.js
+++ 2025-07-29-async-await/07-hanukkah/app.js
@@ -2,14 +2,17 @@
 
 (async () => {
 
-    const getNumberOfCandles = hanukkahSerialDay => {
-        if (hanukkahSerialDay > 8) throw new Error('max day is 8')
-        if (hanukkahSerialDay < 1) throw new Error('min day is 1')
-        return hanukkahSerialDay + 1
+    const getNumberOfCandles = (hanukkahSerialDay, successCallback, errorCallback) => {
+        setTimeout(() => {
+            if (hanukkahSerialDay > 8) return errorCallback('max day is 8')
+            if (hanukkahSerialDay < 1) return errorCallback('min day is 1')
+            successCallback(hanukkahSerialDay + 1)
+        }, 1 * 1000)
     }
 
-    console.log(getNumberOfCandles(6))
-    console.log(getNumberOfCandles(9))
-    console.log(getNumberOfCandles(0))
+    getNumberOfCandles(7, console.log, console.log)
+    getNumberOfCandles(9, console.log, console.log)
+    getNumberOfCandles(0, console.log, console.log)
+
 
 })()
