402d6de56b49f8f748ecb6ae235bf1935c43b493
shaharsol <shahar@gnift.com>
2025-07-29 11:42:06 +0300

    promise hannukah

--- 2025-07-29-async-await/07-hanukkah/app.js
+++ 2025-07-29-async-await/07-hanukkah/app.js
@@ -2,17 +2,55 @@
 
 (async () => {
 
-    const getNumberOfCandles = (hanukkahSerialDay, successCallback, errorCallback) => {
-        setTimeout(() => {
-            if (hanukkahSerialDay > 8) return errorCallback('max day is 8')
-            if (hanukkahSerialDay < 1) return errorCallback('min day is 1')
-            successCallback(hanukkahSerialDay + 1)
-        }, 1 * 1000)
+    // sync version:
+
+    // const getNumberOfCandles = (hanukkahSerialDay) => {
+    //     if (hanukkahSerialDay > 8) throw new Error('max day is 8')
+    //     if (hanukkahSerialDay < 1) throw new Error('min day is 1')
+    //     return hanukkahSerialDay + 1
+    // }
+
+    // try {
+    //     console.log(getNumberOfCandles(7))
+    //     console.log(getNumberOfCandles(9))
+    //     console.log(getNumberOfCandles(0))
+    // }
+    // catch (err) {
+    //     console.error(err)
+    // }
+
+
+    // callback version
+
+    // const getNumberOfCandles = (hanukkahSerialDay, successCallback, errorCallback) => {
+    //     setTimeout(() => {
+    //         if (hanukkahSerialDay > 8) return errorCallback('max day is 8')
+    //         if (hanukkahSerialDay < 1) return errorCallback('min day is 1')
+    //         successCallback(hanukkahSerialDay + 1)
+    //     }, 1 * 1000)
+    // }
+
+    // getNumberOfCandles(7, console.log, console.log)
+    // getNumberOfCandles(9, console.log, console.log)
+    // getNumberOfCandles(0, console.log, console.log)
+
+    // promise version:
+    const getNumberOfCandles = (hanukkahSerialDay) => {
+        return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                if (hanukkahSerialDay > 8) reject('max day is 8')
+                if (hanukkahSerialDay < 1) reject('min day is 1')
+                resolve(hanukkahSerialDay + 1)
+            }, 1 * 1000)
+        })
     }
 
-    getNumberOfCandles(7, console.log, console.log)
-    getNumberOfCandles(9, console.log, console.log)
-    getNumberOfCandles(0, console.log, console.log)
-
+    Promise.allSettled([
+        getNumberOfCandles(7),
+        getNumberOfCandles(9),
+        getNumberOfCandles(0),
+    ])
+        .then(console.log)
+        .catch(console.error)
 
 })()
