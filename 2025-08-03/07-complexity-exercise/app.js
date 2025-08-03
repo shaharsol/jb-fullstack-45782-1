2f745f6226034d30f7ada4502659c5a8a5098dca
shaharsol <shahar@gnift.com>
2025-08-03 14:33:07 +0300

    reduce into {} to get O(n)

---
+++ 2025-08-03/07-complexity-exercise/app.js
@@ -0,0 +1,53 @@
+"use strict";
+
+(async () => {
+
+
+
+    const getData = url => fetch(url).then(response => response.json())
+
+    const fetchUsers = async () => {
+        const { users } = await getData('https://dummyjson.com/users')
+        return users
+    }
+
+    const users = await fetchUsers()
+
+    console.log(
+
+        Object.entries(
+            users.reduce((cumulative, { eyeColor }) => {
+                if (cumulative[eyeColor]) cumulative[eyeColor] += 1
+                else cumulative[eyeColor] = 1
+                return cumulative
+            }, {})
+        )
+            .map(([color, count]) => ({ color, count }))
+
+
+    )
+
+    /*
+    [
+        {
+            color: 'black',
+            count: 20
+        },
+        {
+            color: 'brown',
+            count: 12
+        }
+    ]
+
+    const obj = {
+        black: 20
+        brown: 12
+    }
+
+    obj.black += 1
+    const color = 'black'
+    obj[color] += 1
+
+    */
+})()
+
