2665ca415ad0e6a66334a5c0f67b364ddbb60f8d
shaharsol <shahar@gnift.com>
2025-08-03 11:24:07 +0300

    stats with .length

---
+++ 2025-08-03/04-aggregation/app.js
@@ -0,0 +1,45 @@
+"use strict";
+
+(async () => {
+
+    const getData = url => fetch(url).then(response => response.json())
+
+    const fetchUsers = async () => {
+        const { users } = await getData('https://dummyjson.com/users')
+        return users
+    }
+
+    // generateHTML (process input)
+    const generateUsersHTML = (users) => {
+        const html = users
+            .map(({ firstName, lastName, image }) => `<tr>
+                <td>${firstName} ${lastName}</td>
+                <td><img src="${image}"></td>
+            </tr>`)
+            .join('')
+
+        return html
+    }
+
+    const generateStatsHtml = users => `
+        <p>total: ${users.length}</p>
+    `
+
+    // renderHTML (generate output)
+    const renderHTML = (html, target) => document.getElementById(target).innerHTML = html
+
+    const renderUsersHTML = html => renderHTML(html, 'users-list')
+    const renderStatsHTML = html => renderHTML(html, 'stats')
+
+
+
+    // main...
+    const users = await fetchUsers()
+    const usersHtml = generateUsersHTML(users)
+    renderUsersHTML(usersHtml)
+
+    const statsHTML = generateStatsHtml(users)
+    renderStatsHTML(statsHTML)
+
+
+})()
