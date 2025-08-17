f5777ad77e0f1e720445b474be70e66f32b48529
shaharsol <shahar@gnift.com>
2025-08-17 13:04:51 +0300

    oop

---
+++ 2025-08-17/06-oop/src/car.ts
@@ -0,0 +1,12 @@
+class Car {
+    // member variables (e.g. properties)
+    make: string
+    model: string
+    engineVolume: number
+    yearModel: number
+
+    // methods (e.g. function)
+    ignite() { 
+        console.log('hello')
+    }
+}
