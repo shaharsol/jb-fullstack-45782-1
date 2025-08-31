ae4b82b5552444ff3e3e66d8c4395f82e3f4453f
shaharsol <shahar@gnift.com>
2025-08-31 11:54:24 +0300

    axios

--- 2025-08-31-/01-social-network-homework-solution/src/services/profile.ts
+++ 2025-08-31-/01-social-network-homework-solution/src/services/profile.ts
@@ -1,14 +1,15 @@
+import axios from "axios"
 import type Post from "../models/post"
 
 class ProfileService {
     async getProfile(): Promise<Post[]> {
-        const profile = await fetch(`${import.meta.env.VITE_REST_SERVER_URL}/profile`).then<Post[]>(response => response.json())
-        return profile
+        const response = await axios.get<Post[]>(`${import.meta.env.VITE_REST_SERVER_URL}/profile`)
+        return response.data
     }
 
     async remove(id: string): Promise<boolean> {
-        const deleted = await fetch(`${import.meta.env.VITE_REST_SERVER_URL}/profile/${id}`, { method: 'DELETE' }).then<boolean>(response => response.json())
-        return deleted
+        const response = await axios.delete(`${import.meta.env.VITE_REST_SERVER_URL}/profile/${id}`)
+        return response.data
     }
 }
 
