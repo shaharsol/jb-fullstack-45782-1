fb80a1e6f27656f9cd4f8df09ffcb8fbd70de2c5
shaharsol <shahar@gnift.com>
2025-09-07 10:52:28 +0300

    alota

--- 2025-09-07-auth/01-social-network-spinner-button/src/components/common/spinner-button/SpinnerButton.tsx
+++ 2025-09-07-auth/01-social-network-spinner-button/src/components/common/spinner-button/SpinnerButton.tsx
@@ -5,13 +5,14 @@ interface SpinnerButtonProps {
     buttonText: string
     loadingText: string
     isSubmitting: boolean
+    onClick?: React.MouseEventHandler<HTMLButtonElement>
 }
 export default function SpinnerButton(props: SpinnerButtonProps) {
 
-    const { buttonText, loadingText, isSubmitting } = props
+    const { buttonText, loadingText, isSubmitting, onClick } = props
     return (
         <div className='SpinnerButton'>
-            {!isSubmitting && <button>{buttonText}</button>}
+            {!isSubmitting && <button onClick={onClick}>{buttonText}</button>}
             {isSubmitting && <span>{loadingText}...<i><img src={spinnerSource} /></i></span>}
         </div>
     )
--- 2025-09-07-auth/01-social-network-spinner-button/src/components/follows/follow/Follow.tsx
+++ 2025-09-07-auth/01-social-network-spinner-button/src/components/follows/follow/Follow.tsx
@@ -1,5 +1,7 @@
+import { useState } from 'react'
 import type User from '../../../models/user'
 import followingService from '../../../services/following'
+import SpinnerButton from '../../common/spinner-button/SpinnerButton'
 import './Follow.css'
 
 interface FollowProps {
@@ -11,15 +13,20 @@ export default function Follow(props: FollowProps) {
 
     const { user: { id, name }, isFollowing, unfollow } = props
 
+    const [isSubmitting, setIsSubmitting] = useState<boolean>(false)
+
 
     async function unfollowMe() {
         try {
+            setIsSubmitting(true)
             if (unfollow) {
                 await followingService.unfollow(id)
                 unfollow(id)
             }
         } catch (e) {
             alert(e)
+        } finally {
+            setIsSubmitting(false)
         }
     }
 
@@ -28,7 +35,12 @@ export default function Follow(props: FollowProps) {
             <img src="https://static.vecteezy.com/system/resources/previews/036/594/092/non_2x/man-empty-avatar-photo-placeholder-for-social-networks-resumes-forums-and-dating-sites-male-and-female-no-photo-images-for-unfilled-user-profile-free-vector.jpg" />
             <div>{name}</div>
             <div>
-                {isFollowing && <button onClick={unfollowMe}>unfollow</button>}
+                {isFollowing && <SpinnerButton
+                    onClick={unfollowMe}
+                    buttonText='unfollow'
+                    loadingText='unfollowing'
+                    isSubmitting={isSubmitting}
+                />}
             </div>
         </div>
     )
