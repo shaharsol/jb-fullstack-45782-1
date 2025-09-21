5680cbf0a54b5b9e8a03582302ae7ce2d7192042
shaharsol <shahar@gnift.com>
2025-09-21 16:16:18 +0300

    server

--- 2025-09-21-node.js/14-basic-server-plus/server.js
+++ 2025-09-21-node.js/14-basic-server-plus/server.js
@@ -17,7 +17,33 @@ const users = [
     }
 ]
 
-const requestHandler = (request, response) => response.end('<h3>hello world</h3>')
+const requestHandler = (request, response) => {
+    console.log(`url is ${request.url} and method is ${request.method}`)
+
+    switch (request.url) {
+        case '/':
+            switch (request.method) {
+                case 'GET':
+                    response.end('hello world')
+                    break;
+                default:
+                    response.writeHead(404).end('not found')
+            }
+            break;
+        case '/users':
+            switch (request.method) {
+                case 'GET':
+                    response.setHeader('Content-Type', 'application/json')
+                    response.end(JSON.stringify(users))
+                    break;
+                default:
+                    response.writeHead(404).end('not found')
+            }
+            break;
+        default:
+            response.writeHead(404).end('not found')
+    }
+}
 
 
 createServer(requestHandler).listen(PORT, () => console.log(`server started on port ${PORT}...`))
