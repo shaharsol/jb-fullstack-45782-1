117e74dfb84c797152f480eef08273a9bdfb3ecb
shaharsol <shahar@gnift.com>
2025-09-28 15:39:07 +0300

    ts

---
+++ 2025-09-28-express/08-express-with-typescript/src/app.ts
@@ -0,0 +1,11 @@
+import express, { NextFunction, Request, Response } from 'express'
+
+const app = express()
+
+function logRequest(req: Request, res: Response, next: NextFunction) {
+    console.log('logging request')
+}
+
+app.use('/', logRequest)
+
+app.listen(3000)
