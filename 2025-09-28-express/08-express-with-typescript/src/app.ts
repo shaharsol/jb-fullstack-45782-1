d61b8cf72a0c397562a8e2ba08929dd9072790f1
shaharsol <shahar@gnift.com>
2025-09-28 15:49:06 +0300

    how to run expre4ss in ts

--- 2025-09-28-express/08-express-with-typescript/src/app.ts
+++ 2025-09-28-express/08-express-with-typescript/src/app.ts
@@ -3,7 +3,7 @@ import express, { NextFunction, Request, Response } from 'express'
 const app = express()
 
 function logRequest(req: Request, res: Response, next: NextFunction) {
-    console.log('logging request')
+    console.log('logging request very very very good')
 }
 
 app.use('/', logRequest)
