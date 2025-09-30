6e32282e1772c7ca5cca870280f5cc6398bf2c69
shaharsol <shahar@gnift.com>
2025-09-30 10:37:34 +0300

    config

---
+++ 2025-09-30/02-weezer-server-config/src/app.ts
@@ -0,0 +1,25 @@
+import express from 'express'
+import logger from './middlewares/error/logger';
+import responder from './middlewares/error/responder';
+import notFound from './middlewares/not-found';
+import profileRouter from './routers/profile'
+import config from 'config'
+
+const app = express()
+
+const port = config.get<number>('app.port')
+const appName = config.get<string>('app.name')
+const secret = config.get<string>('app.secret')
+
+console.log(`app secret is ${secret}`)
+// load routers
+app.use('/profile', profileRouter)
+
+// not found
+app.use(notFound)
+
+// error middlewares
+app.use(logger)
+app.use(responder)
+
+app.listen(port, () => console.log(`${appName} started on port ${port}`))
