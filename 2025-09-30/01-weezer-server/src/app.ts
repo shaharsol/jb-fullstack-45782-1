196a99d9d9b4e8092d0e5d053187aabd4d452265
shaharsol <shahar@gnift.com>
2025-09-30 10:08:40 +0300

    basic app structure

--- 2025-09-30/01-weezer-server/src/app.ts
+++ 2025-09-30/01-weezer-server/src/app.ts
@@ -2,10 +2,14 @@ import express from 'express'
 import logger from './middlewares/error/logger';
 import responder from './middlewares/error/responder';
 import notFound from './middlewares/not-found';
+import profileRouter from './routers/profile'
 
 const PORT = 3000;
 
 const app = express()
+
+// load routers
+app.use('/profile', profileRouter)
 
 // not found
 app.use(notFound)
