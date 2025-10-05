113a3751bc6511ac31cc95a5c0a84fd3cf8c28b2
shaharsol <shahar@gnift.com>
2025-10-05 13:28:24 +0300

    feed infra

---
+++ 2025-10-05/06-weezer-server-sequelize-profile-validation/src/controllers/feed/controller.ts
@@ -0,0 +1,5 @@
+import { NextFunction, Request, Response } from "express";
+
+export async function getFeed(req: Request, res: Response, next: NextFunction) {
+    const userId = '1230ae30-dc4f-4752-bd84-092956f5c633'
+}
