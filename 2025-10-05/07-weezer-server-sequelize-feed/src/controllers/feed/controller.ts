0dd78abe20145b531fa13ccad8e28b84ca3199c4
shaharsol <shahar@gnift.com>
2025-10-05 14:31:05 +0300

    feed complete

--- 2025-10-05/07-weezer-server-sequelize-feed/src/controllers/feed/controller.ts
+++ 2025-10-05/07-weezer-server-sequelize-feed/src/controllers/feed/controller.ts
@@ -6,26 +6,31 @@ import Comment from "../../models/Comment";
 import postIncludes from "../common/post-includes";
 
 export async function getFeed(req: Request, res: Response, next: NextFunction) {
-    const userId = '1230ae30-dc4f-4752-bd84-092956f5c633'
+    try {
+        const userId = '1230ae30-dc4f-4752-bd84-092956f5c633'
 
-    const { following } = await User.findByPk(userId, {
-        include: {
-            model: User,
-            as: 'following',
-            include: [{
-                model: Post,
-                ...postIncludes
-            }]
-        },
-    })
+        const { following } = await User.findByPk(userId, {
+            include: {
+                model: User,
+                as: 'following',
+                include: [{
+                    model: Post,
+                    ...postIncludes
+                }]
+            },
+        })
 
-    const feed = following
-        .reduce((cumulative: Post[], { posts }) => {
-            return [...posts, ...cumulative]
-        }, [])
-        .sort((a: Post, b: Post) => a.createdAt < b.createdAt ? 1 : -1)
+        const feed = following
+            .reduce((cumulative: Post[], { posts }) => {
+                return [...posts, ...cumulative]
+            }, [])
+            .sort((a: Post, b: Post) => a.createdAt < b.createdAt ? 1 : -1)
 
-    res.json(feed)
+        res.json(feed)
+
+    } catch (e) {
+        next(e)
+    }
 
 
     // how i can query the database WITHOUT sequelize help
