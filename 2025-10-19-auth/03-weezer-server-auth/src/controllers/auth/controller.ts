70fb9560bc7ce8b6ada33ba9e4c22c91352f38bd
shaharsol <shahar@gnift.com>
2025-10-19 11:57:51 +0300

    authentication infra

---
+++ 2025-10-19-auth/03-weezer-server-auth/src/controllers/auth/controller.ts
@@ -0,0 +1,17 @@
+import { NextFunction, Request, Response } from "express";
+
+export async function signup(req: Request, res: Response, next: NextFunction) {
+    try {
+
+    } catch (e) {
+        next(e)
+    }
+}
+
+export async function login(req: Request, res: Response, next: NextFunction) {
+    try {
+
+    } catch (e) {
+        next(e)
+    }
+}
