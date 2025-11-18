106c9a9874f4536ab69c839238d85a32f68367b3
shaharsol <shahar@gnift.com>
2025-11-18 12:04:19 +0200

    another beautiful test

--- 2025-11-18-tests/01-tests/backend/src/middlewares/enforce-auth.test.ts
+++ 2025-11-18-tests/01-tests/backend/src/middlewares/enforce-auth.test.ts
@@ -20,4 +20,23 @@ describe('enforce auth unit testing', () => {
 
         // [ [ {status: 401, message: ''} ] ]
     })
+    test('calls next with status 401 when bearer keyword is mispelled', () => {
+        // mocking
+        const request = {
+            headers: {
+                authorization: 'Baerer eyfhjhgjkdfhkjgdfh'
+            },
+            get: (key: string) => 'Baerer eyfhjhgjkdfhkjgdfh'
+        } as Request
+        const response = {} as Response
+        const next = jest.fn(err => {})
+        enforceAuth(request, response, next)
+        expect(next.mock.calls.length).toBe(1)
+        expect(next.mock.calls[0][0]).toEqual({
+            status: 401,
+            message: 'missing Bearer keyword'
+        })
+
+        // [ [ {status: 401, message: ''} ] ]
+    })
 })
